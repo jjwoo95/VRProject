a4aab9ffcc271ea5cb781b77d8ffc8e31e93f50e
Jay Woo <jjwoo95@gmail.com>
2018-04-19 08:38:10 -0700

    Deleted IPD scene. In changePlayerSizeLevel roof is now blown off when player gets too big.

---
+++ Assets/Scripts/BlowOffRoof.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowOffRoof : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float thrust = 30f;
+    public GameObject cameraRig;
+    private bool alreadyBlownOff = false;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(cameraRig.transform.localScale.x > 1.9f && alreadyBlownOff == false)
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
+            rb.AddForce(transform.up * thrust);
+            alreadyBlownOff = true;
+        }
+
+        if(alreadyBlownOff == true)
+        {
+            transform.Rotate(1f, 1f, 1f);
+        }
+	}
+}
