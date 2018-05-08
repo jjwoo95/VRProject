a61a7f1e03c27ae271e69809ce51babde5e521d7
Jay Woo <jjwoo95@gmail.com>
2018-05-08 08:42:31 -0700

    Added an object which is grabable depending on the player's size.

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,43 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PickUpObject : MonoBehaviour
+    {
+        // player's current size
+        public GameObject cameraRig;
+
+        // object which is being grabbed
+        private VRTK_InteractableObject interactableObject;
+
+        // size player must ne to grab object
+        public float requiredPlayerSize = 1.1f;
+
+        // Use this for initialization
+        void Start()
+        {
+            // get component to grab an object
+            interactableObject = GetComponent<VRTK_InteractableObject>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // check to see if player is large enough to grab object
+            if(cameraRig.transform.localScale.y >= requiredPlayerSize)
+            {
+                // allow player to grab object
+                interactableObject.isGrabbable = true;
+            }
+
+            // check to see if player is to small to grab object
+            else if (cameraRig.transform.localScale.y < requiredPlayerSize)
+            {
+                // don't allow player to grab object
+                interactableObject.isGrabbable = false;
+            }
+        }
+    }
+}
